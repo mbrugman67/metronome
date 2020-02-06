00a1fec5d6b87bd5e55d22a72b17e10b9169beb6
git <mattb@linux.com>
2020-02-06 13:02:24 -0600

    lcd stuffs

--- avr/lcd/lcd.cpp
+++ avr/lcd/lcd.cpp
@@ -1,136 +1,116 @@
-/*
-TextLCD::TextLCD(PinName rs, PinName e, PinName d4, PinName d5,
-                 PinName d6, PinName d7, LCDType type) : _rs(rs),
-        _e(e), _d(d4, d5, d6, d7),
-        _type(type) {
- 
-    _e  = 1;
-    _rs = 0;            // command mode
- 
-    wait(0.015);        // Wait 15ms to ensure powered up
- 
-    // send "Display Settings" 3 times (Only top nibble of 0x30 as we've got 4-bit bus)
-    for (int i=0; i<3; i++) {
-        writeByte(0x3);
-        wait(0.00164);  // this command takes 1.64ms, so wait for it
+#include "lcd.h"
+#include "../sys/ioDefinitions.h"
+
+#define MAX_LINE_LENGTH     20
+#define MAX_LINE_COUNT      4
+
+void lcd::init()
+{
+    LCD_E_ON();
+    LCD_RS_OFF();
+
+    // wait for LCD to be sure it's powered up
+    _delay_ms(15.0);   
+
+    for (size_t ii = 0; ii < 3; ++ii)
+    {
+        this->writeByte(0x03);
+        _delay_ms(1.64);
     }
-    writeByte(0x2);     // 4-bit mode
-    wait(0.000040f);    // most instructions take 40us
+
+    this->writeByte(0x02);     // 4-bit mode
+    _delay_us(40.0);    // most instructions take 40us
  
-    writeCommand(0x28); // Function set 001 BW N F - -
-    writeCommand(0x0C);
-    writeCommand(0x6);  // Cursor Direction and Display Shift : 0000 01 CD S (CD 0-left, 1-right S(hift) 0-no, 1-yes
-    cls();
+    this->sendChar(0x28); // Function set 001 BW N F - -
+    this->sendChar(0x0C);
+    this->sendChar(0x6); 
+
+    strncpy(blank, "                    ", 20);
+
+    if (dbg)    printf_P(PSTR("LCD init done\n"));
 }
- 
-void TextLCD::character(int column, int row, int c) {
-    int a = address(column, row);
-    writeCommand(a);
-    writeData(c);
+
+void lcd::clearAll()
+{
+    this->sendCmd(0x01); // clear the whole thang
+    _delay_ms(1.64);     // This command takes 1.64 ms
+
+    if (dbg)    printf_P(PSTR("LCD total clear done\n"));
 }
- 
-void TextLCD::cls() {
-    writeCommand(0x01); // cls, and set cursor to 0
-    wait(0.00164f);     // This command takes 1.64 ms
-    locate(0, 0);
+void lcd::clearLine(lcd_line_t line)
+{
+    this->writeLine(line, blank);
 }
- 
-void TextLCD::locate(int column, int row) {
-    _column = column;
-    _row = row;
+
+void lcd::writeLine(lcd_line_t line, char* text)
+{
+    this->writeLineAt(line, 0, text);
 }
- 
-int TextLCD::_putc(int value) {
-    if (value == '\n') {
-        _column = 0;
-        _row++;
-        if (_row >= rows()) {
-            _row = 0;
-        }
-    } else {
-        character(_column, _row, value);
-        _column++;
-        if (_column >= columns()) {
-            _column = 0;
-            _row++;
-            if (_row >= rows()) {
-                _row = 0;
-            }
-        }
+
+void lcd::writeLineAt(lcd_line_t line, uint8_t posn, char* text)
+{
+    char* c = text;
+    size_t len = strlen(text);
+
+    if (len + posn > MAX_LINE_LENGTH)
+    {
+        len = MAX_LINE_LENGTH - posn;
     }
-    return value;
-}
- 
-int TextLCD::_getc() {
-    return -1;
-}
- 
-void TextLCD::writeByte(int value) {
-    _d = value >> 4;
-    wait(0.000040f); // most instructions take 40us
-    _e = 0;
-    wait(0.000040f);
-    _e = 1;
-    _d = value >> 0;
-    wait(0.000040f);
-    _e = 0;
-    wait(0.000040f);  // most instructions take 40us
-    _e = 1;
-}
- 
-void TextLCD::writeCommand(int command) {
-    _rs = 0;
-    writeByte(command);
-}
- 
-void TextLCD::writeData(int data) {
-    _rs = 1;
-    writeByte(data);
-}
- 
-int TextLCD::address(int column, int row) {
-    switch (_type) {
-        case LCD20x4:
-            switch (row) {
-                case 0:
-                    return 0x80 + column;
-                case 1:
-                    return 0xc0 + column;
-                case 2:
-                    return 0x94 + column;
-                case 3:
-                    return 0xd4 + column;
-            }
-        case LCD16x2B:
-            return 0x80 + (row * 40) + column;
-        case LCD16x2:
-        case LCD20x2:
-        default:
-            return 0x80 + (row * 0x40) + column;
+
+    // locate cursor at beginning of line
+    this->sendCmd((uint8_t)line + posn);
+
+    while(len)
+    {
+        this->sendChar(*c);
+        ++c;
+        --len;
     }
+
+    printf_P(PSTR("writeLineAt(%d, %d, >%s<)\n"), (uint8_t)line, posn, text);
 }
- 
-int TextLCD::columns() {
-    switch (_type) {
-        case LCD20x4:
-        case LCD20x2:
-            return 20;
-        case LCD16x2:
-        case LCD16x2B:
-        default:
-            return 16;
+void lcd::writeCharAt(lcd_line_t line, uint8_t posn, char c)
+{
+    if (posn > MAX_LINE_LENGTH)
+    {
+        posn = MAX_LINE_LENGTH;
     }
+
+    this->sendCmd((uint8_t)line + posn);
+    this->sendChar(c);
+
+    printf_P(PSTR("writeCharAt(%d, %d, >%c<)\n"), (uint8_t)line, posn, c);
 }
- 
-int TextLCD::rows() {
-    switch (_type) {
-        case LCD20x4:
-            return 4;
-        case LCD16x2:
-        case LCD16x2B:
-        case LCD20x2:
-        default:
-            return 2;
-    }
+
+void lcd::writeByte(uint8_t b)
+{
+    // high nibble first
+    LCD_D4_SET(GETBIT(b, 4));
+    LCD_D5_SET(GETBIT(b, 5));
+    LCD_D6_SET(GETBIT(b, 6));
+    LCD_D7_SET(GETBIT(b, 7));
+    _delay_us(40.0);
+
+    this->toggleELine();
+
+    // high nibble first
+    LCD_D4_SET(GETBIT(b, 0));
+    LCD_D5_SET(GETBIT(b, 1));
+    LCD_D6_SET(GETBIT(b, 2));
+    LCD_D7_SET(GETBIT(b, 3));
+    _delay_us(40.0);
+
+    this->toggleELine();
 }
-*/
+
+void lcd::sendChar(char c)
+{
+    LCD_RS_ON();
+    this->writeByte(c);
+}
+
+void lcd::sendCmd(uint8_t cmd)
+{
+    LCD_RS_OFF();
+    this->writeByte(cmd);
+}
--- avr/lcd/lcd.h
+++ avr/lcd/lcd.h
@@ -1,137 +1,44 @@
-/*
-TextLCD::TextLCD(PinName rs, PinName e, PinName d4, PinName d5,
-                 PinName d6, PinName d7, LCDType type) : _rs(rs),
-        _e(e), _d(d4, d5, d6, d7),
-        _type(type) {
- 
-    _e  = 1;
-    _rs = 0;            // command mode
- 
-    wait(0.015);        // Wait 15ms to ensure powered up
- 
-    // send "Display Settings" 3 times (Only top nibble of 0x30 as we've got 4-bit bus)
-    for (int i=0; i<3; i++) {
-        writeByte(0x3);
-        wait(0.00164);  // this command takes 1.64ms, so wait for it
+#ifndef LCD_H_
+#define LCD_H_
+
+#include <util/delay.h>
+#include "../project.h"
+
+enum lcd_line_t
+{
+    LINE_1 = 0x80,
+    LINE_2 = 0xc0,
+    LINE_3 = 0x94,
+    LINE_4 = 0xd4 
+};
+class lcd
+{
+public: 
+    lcd(bool debug = false) : dbg(debug) {}
+    virtual ~lcd() {}
+
+    void init();
+
+    void clearAll();
+    void clearLine(lcd_line_t line);
+
+    void writeLine(lcd_line_t line, char* text);
+    void writeLineAt(lcd_line_t line, uint8_t posn, char* text);
+    void writeCharAt(lcd_line_t line, uint8_t posn, char c);
+
+private:
+    bool dbg;
+    char blank[20];
+    void writeByte(uint8_t b);
+    void sendChar(char c);
+    void sendCmd(uint8_t cmd);
+
+    inline void toggleELine()
+    {
+        LCD_E_OFF();
+        _delay_us(40);
+        LCD_E_ON();
     }
-    writeByte(0x2);     // 4-bit mode
-    wait(0.000040f);    // most instructions take 40us
- 
-    writeCommand(0x28); // Function set 001 BW N F - -
-    writeCommand(0x0C);
-    writeCommand(0x6);  // Cursor Direction and Display Shift : 0000 01 CD S (CD 0-left, 1-right S(hift) 0-no, 1-yes
-    cls();
-}
- 
-void TextLCD::character(int column, int row, int c) {
-    int a = address(column, row);
-    writeCommand(a);
-    writeData(c);
-}
- 
-void TextLCD::cls() {
-    writeCommand(0x01); // cls, and set cursor to 0
-    wait(0.00164f);     // This command takes 1.64 ms
-    locate(0, 0);
-}
- 
-void TextLCD::locate(int column, int row) {
-    _column = column;
-    _row = row;
-}
- 
-int TextLCD::_putc(int value) {
-    if (value == '\n') {
-        _column = 0;
-        _row++;
-        if (_row >= rows()) {
-            _row = 0;
-        }
-    } else {
-        character(_column, _row, value);
-        _column++;
-        if (_column >= columns()) {
-            _column = 0;
-            _row++;
-            if (_row >= rows()) {
-                _row = 0;
-            }
-        }
-    }
-    return value;
-}
- 
-int TextLCD::_getc() {
-    return -1;
-}
- 
-void TextLCD::writeByte(int value) {
-    _d = value >> 4;
-    wait(0.000040f); // most instructions take 40us
-    _e = 0;
-    wait(0.000040f);
-    _e = 1;
-    _d = value >> 0;
-    wait(0.000040f);
-    _e = 0;
-    wait(0.000040f);  // most instructions take 40us
-    _e = 1;
-}
- 
-void TextLCD::writeCommand(int command) {
-    _rs = 0;
-    writeByte(command);
-}
- 
-void TextLCD::writeData(int data) {
-    _rs = 1;
-    writeByte(data);
-}
- 
-int TextLCD::address(int column, int row) {
-    switch (_type) {
-        case LCD20x4:
-            switch (row) {
-                case 0:
-                    return 0x80 + column;
-                case 1:
-                    return 0xc0 + column;
-                case 2:
-                    return 0x94 + column;
-                case 3:
-                    return 0xd4 + column;
-            }
-        case LCD16x2B:
-            return 0x80 + (row * 40) + column;
-        case LCD16x2:
-        case LCD20x2:
-        default:
-            return 0x80 + (row * 0x40) + column;
-    }
-}
- 
-int TextLCD::columns() {
-    switch (_type) {
-        case LCD20x4:
-        case LCD20x2:
-            return 20;
-        case LCD16x2:
-        case LCD16x2B:
-        default:
-            return 16;
-    }
-}
- 
-int TextLCD::rows() {
-    switch (_type) {
-        case LCD20x4:
-            return 4;
-        case LCD16x2:
-        case LCD16x2B:
-        case LCD20x2:
-        default:
-            return 2;
-    }
-}
-*/
- 
+};
+
+#endif
